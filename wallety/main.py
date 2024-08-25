50bb5d55cc29407a06551491df96cb696805f7c5
Fernando Salom <fer@rudo.es>
2024-08-25 12:26:19 +0200

    Add main

--- wallety/main.py
+++ wallety/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from wallety.driving.api.user.user_api_adapter import router
+from wallety.driving.api.crypto.crypto_api_adapter import router
 
 app = FastAPI()
 app.include_router(router)
