e25d601afadb98eb0869681d957a7f3533e1f4de
Fernando Salom <fer@rudo.es>
2025-05-16 20:26:09 +0200

    Remove unnecessary properties

--- wallety/domain/entities/crypto.py
+++ wallety/domain/entities/crypto.py
@@ -2,14 +2,12 @@
 class Crypto:
     def __init__(self,
                  crypto_id: str,
-                 rank: str,
                  symbol: str,
                  name: str,
                  price_usd: float,
-                 market_cap: float):
+                 market_cap_usd: float):
         self.id = crypto_id
-        self.rank = rank
         self.symbol = symbol
         self.name = name
         self.price_usd = price_usd
-        self.market_cap = market_cap
+        self.market_cap_usd = market_cap_usd
