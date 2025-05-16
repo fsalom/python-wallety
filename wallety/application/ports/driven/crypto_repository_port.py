f55cff5c00e9af5fe3fd5328f0dfde360ebc9acd
Fernando Salom <fer@rudo.es>
2025-05-16 20:25:44 +0200

    Update crypto_repository_port.py

--- wallety/application/ports/driven/crypto_repository_port.py
+++ wallety/application/ports/driven/crypto_repository_port.py
@@ -1,13 +1,30 @@
 from abc import ABC, abstractmethod
+from typing import List, Optional
 
 from wallety.domain.entities.crypto import Crypto
+from wallety.domain.entities.historical_price import HistoricalPrice
+from wallety.domain.entities.market import Market
 
 
 class CryptoRepositoryPort(ABC):
     @abstractmethod
-    def top100(self) -> [Crypto]:
+    def get_list(self) -> [Crypto]:
         pass
 
     @abstractmethod
     def get(self, coin) -> Crypto:
+        pass
+
+    @abstractmethod
+    def get_markets(self, id) -> List[Market]:
+        pass
+
+    @abstractmethod
+    def get_historical_price(
+            self,
+            id: str,
+            interval: str,
+            start: Optional[int] = None,
+            end: Optional[int] = None
+    ) -> List[HistoricalPrice]:
         pass
