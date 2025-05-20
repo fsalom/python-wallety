5dfe3336164cdfd925b827e7890284884fc33d1e
Fernando Salom <fer@rudo.es>
2025-05-20 16:44:16 +0200

    Add notification container to di

---
+++ notifications/infrastructure/di/notification/container.py
@@ -0,0 +1,21 @@
+from dependency_injector import containers, providers
+
+from notifications.application.services.push_notification_service import PushNotificationService
+from notifications.driven.firebase.adapter import FirebaseRepositoryAdapter
+from notifications.driving.api_rest.v1.notification.mapper import NotificationAPIMapper
+
+
+class NotificationContainer(containers.DeclarativeContainer):
+    config = providers.Configuration()
+    wiring_config = containers.WiringConfiguration(modules=["driving.api_rest.v1.notification.adapter"])
+
+    firebase_repository = providers.Factory(FirebaseRepositoryAdapter)
+
+    service = providers.Factory(
+        PushNotificationService,
+        firebase_repository=firebase_repository,
+    )
+
+    api_mapper = providers.Factory(
+        NotificationAPIMapper
+    )
