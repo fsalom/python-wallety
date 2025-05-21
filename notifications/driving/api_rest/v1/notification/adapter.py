7efa4fb9d6a34cd5597748a1e6c682ab3e442a10
Fernando Salom <fer@rudo.es>
2025-05-21 20:21:14 +0200

    Add notification adapter

---
+++ notifications/driving/api_rest/v1/notification/adapter.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from starlette import status
+from starlette.responses import JSONResponse
+
+from notifications.application.ports.driving.push_notification_service_port import PushNotificationServicePort
+from notifications.driving.api_rest.v1.notification.mapper import NotificationAPIMapper
+from notifications.driving.api_rest.v1.notification.models import NotificationDataRequest
+
+fcm_router = APIRouter()
+
+
+@fcm_router.post('/notification/send')
+@inject
+async def send_notification(notification_request: NotificationDataRequest,
+                            service: PushNotificationServicePort = Depends(Provide[NotificationContainer.service]),
+                            mapper: NotificationAPIMapper = Depends(Provide[NotificationContainer.api_mapper])):
+    notification = mapper.from_notification_dto_to_domain(notification_request)
+    if notification.recipients.count() > 1:
+        service.send_bulk(notification, notification.recipients)
+    else:
+        service.send(notification, notification.recipients[0])
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=None
+    )
