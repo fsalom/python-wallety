e2747e270ca5c0434e0ccd117fa3400d610de115
Fernando Salom <fer@rudo.es>
2025-05-24 18:08:44 +0200

    Update crypto_api_mapper.py

--- wallety/driving/api/crypto/crypto_api_mapper.py
+++ wallety/driving/api/crypto/crypto_api_mapper.py
@@ -1,11 +1,10 @@
-from wallety.domain.entities.crypto import Crypto
+from wallety.domain.crypto import Crypto
 from wallety.driving.api.crypto.models.crypto_dto import CryptoDTO
 
 
 class CryptoMapper:
     def from_entities_to_models(self, cryptos: [Crypto]) -> [CryptoDTO]:
-        cryptos = [self.from_entity_to_model(**item) for item in cryptos]
-        return cryptos
+        return [self.from_entity_to_model(crypto) for crypto in cryptos]
 
     @staticmethod
     def from_entity_to_model(crypto: Crypto) -> CryptoDTO:
