9fcee301ccde929e889da795638b04054c92cad0
Fernando Salom <fer@rudo.es>
2025-05-24 18:08:33 +0200

    Add new mapper for market and historical price

--- wallety/driven/api/crypto/coincap_mapper.py
+++ wallety/driven/api/crypto/coincap_mapper.py
@@ -1,7 +1,13 @@
-from wallety.domain.entities.crypto import Crypto
+from typing import List
+
+from wallety.domain.crypto import Crypto
+from wallety.domain.historical_price import HistoricalPrice
+from wallety.domain.market import Market
 from wallety.driven.api.crypto.dto.coincap_data_list_dto import CoincapDataListDTO
 from wallety.driven.api.crypto.dto.coincap_data_single_dto import CoincapDataSingleDTO
 from wallety.driven.api.crypto.dto.coincap_dto import CoincapDTO
+from wallety.driven.api.crypto.dto.historical_price_data_dto import HistoricalPriceData
+from wallety.driven.api.crypto.dto.market_data_list_dto import MarketDataResponse
 
 
 class CoincapMapper:
@@ -15,9 +21,33 @@ class CoincapMapper:
     @staticmethod
     def from_model_to_entity(crypto_dto: CoincapDTO) -> Crypto:
         return Crypto(crypto_id=crypto_dto.id,
-                      rank=crypto_dto.rank,
                       symbol=crypto_dto.symbol,
                       name=crypto_dto.name,
                       price_usd=crypto_dto.price_usd,
-                      market_cap=crypto_dto.market_cap_usd
-                      )
+                      market_cap_usd=crypto_dto.market_cap_usd)
+
+    @staticmethod
+    def to_domains_market(data: MarketDataResponse) -> List[Market]:
+        return [
+            Market(
+                exchange_id=market.exchange_id,
+                base_id=market.base_id,
+                quote_id=market.quote_id,
+                base_symbol=market.base_symbol,
+                quote_symbol=market.quote_symbol,
+                volume_usd_24hr=market.volume_usd_24hr,
+                price_usd=market.price_usd,
+                volume_percent=market.volume_percent
+            )
+            for market in data.data
+        ]
+
+    @staticmethod
+    def to_domains_historical_price(prices: List[HistoricalPriceData]) -> List[HistoricalPrice]:
+        return [
+            HistoricalPrice(
+                price_usd=price.price_usd,
+                date=price.date
+            )
+            for price in prices
+        ]
