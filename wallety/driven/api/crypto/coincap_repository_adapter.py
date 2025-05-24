d16d2e0ea589233f4c49aef500fd5a7cbc1ddbd6
Fernando Salom <fer@rudo.es>
2025-05-24 18:08:12 +0200

    Add new functionalities for adapter

--- wallety/driven/api/crypto/coincap_repository_adapter.py
+++ wallety/driven/api/crypto/coincap_repository_adapter.py
@@ -1,10 +1,16 @@
+import os
+from typing import List, Optional
+
 import httpx
-from fastapi import HTTPException
 from wallety.application.ports.driven.crypto_repository_port import CryptoRepositoryPort
-from wallety.domain.entities.crypto import Crypto
+from wallety.domain.crypto import Crypto
+from wallety.domain.historical_price import HistoricalPrice
+from wallety.domain.market import Market
 from wallety.driven.api.crypto.coincap_mapper import CoincapMapper
 from wallety.driven.api.crypto.dto.coincap_data_list_dto import CoincapDataListDTO
 from wallety.driven.api.crypto.dto.coincap_data_single_dto import CoincapDataSingleDTO
+from wallety.driven.api.crypto.dto.historical_data_list_dto import HistoricalData
+from wallety.driven.api.crypto.dto.market_data_list_dto import MarketDataResponse
 
 
 class CryptoAPIRepository(CryptoRepositoryPort):
@@ -12,30 +18,52 @@ class CryptoAPIRepository(CryptoRepositoryPort):
     def __init__(self, mapper: CoincapMapper):
         self.mapper = mapper
 
-    async def top100(self) -> [Crypto]:
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.get("https://api.coincap.io/v2/assets")
-                response.raise_for_status()
-                json = response.json()
-                crypto_data = CoincapDataListDTO(**json)
-                return self.mapper.from_data_list_to_list_entities(crypto_data)
-        except httpx.HTTPStatusError as exc:
-            raise HTTPException(status_code=exc.response.status_code,
-                                detail=f"Error fetching data: {exc.response.text}")
-        except httpx.RequestError as exc:
-            raise HTTPException(status_code=500, detail=f"An error occurred while requesting data: {str(exc)}")
+    async def get_list(self) -> List[Crypto]:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"https://rest.coincap.io/v3/assets?apiKey={os.getenv('API_KEY')}")
+            response.raise_for_status()
 
-    async def get(self, coin) -> Crypto:
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.get(f"https://api.coincap.io/v2/assets/{coin}")
-                response.raise_for_status()
-                json = response.json()
-                crypto_dto = CoincapDataSingleDTO(**json)
-                return self.mapper.from_data_single_to_entity(crypto_dto)
-        except httpx.HTTPStatusError as exc:
-            raise HTTPException(status_code=exc.response.status_code,
-                                detail=f"Error fetching data: {exc.response.text}")
-        except httpx.RequestError as exc:
-            raise HTTPException(status_code=500, detail=f"An error occurred while requesting data: {str(exc)}")
+            data = CoincapDataListDTO(**response.json())
+            return self.mapper.from_data_list_to_list_entities(data)
+
+    async def get(self, id) -> Crypto:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"https://rest.coincap.io/v3/assets/{id}/?apiKey={os.getenv('API_KEY')}")
+            response.raise_for_status()
+
+            info = CoincapDataSingleDTO(**response.json())
+            return self.mapper.from_data_single_to_entity(info)
+
+    async def get_markets(self, id) -> List[Market]:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                f"https://rest.coincap.io/v3/assets/{id}/markets/?apiKey={os.getenv('API_KEY')}")
+            response.raise_for_status()
+
+            info = MarketDataResponse(**response.json())
+            return self.mapper.to_domains_market(info)
+
+    async def get_historical_price(
+            self,
+            id: str,
+            interval: str,
+            start: Optional[int] = None,
+            end: Optional[int] = None
+    ) -> List[HistoricalPrice]:
+        url = f"https://rest.coincap.io/v3/assets/{id}/history"
+        params = {
+            "interval": interval,
+            "apiKey": os.getenv("API_KEY")
+        }
+
+        if start:
+            params["start"] = start
+        if end:
+            params["end"] = end
+
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params=params)
+            response.raise_for_status()
+
+            info = HistoricalData(**response.json())
+            return self.mapper.to_domains_historical_price(info.data)
