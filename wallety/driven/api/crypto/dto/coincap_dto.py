383c6e0b3ca2ab99b910c53f71a1a1cde6d7f2fe
Fernando Salom <fer@rudo.es>
2025-05-24 18:07:52 +0200

    Change type of property

--- wallety/driven/api/crypto/dto/coincap_dto.py
+++ wallety/driven/api/crypto/dto/coincap_dto.py
@@ -1,14 +1,11 @@
-from typing import List
-
 from pydantic import BaseModel, Field
 
 
 class CoincapDTO(BaseModel):
     id: str
-    rank: str
     symbol: str
     name: str
-    price_usd: str = Field(..., alias='priceUsd')
-    market_cap_usd: str = Field(..., alias='marketCapUsd')
+    price_usd: float = Field(..., alias='priceUsd')
+    market_cap_usd: float = Field(..., alias='marketCapUsd')
 
 
