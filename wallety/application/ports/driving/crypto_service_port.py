860ced6f67902bb46096b346d07b1024af7f281c
Fernando Salom <fer@rudo.es>
2025-05-26 20:10:29 +0200

    Fix routes to entities and new functions name

--- wallety/application/ports/driving/crypto_service_port.py
+++ wallety/application/ports/driving/crypto_service_port.py
@@ -1,26 +1,26 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
 
-from wallety.domain.entities.crypto import Crypto
-from wallety.domain.entities.historical_price import HistoricalPrice
-from wallety.domain.entities.market import Market
+from wallety.domain.crypto import Crypto
+from wallety.domain.historical_price import HistoricalPrice
+from wallety.domain.market import Market
 
 
 class CryptoServicePort(ABC):
     @abstractmethod
-    def get_coins(self) -> List[Crypto]:
+    def get_list(self) -> List[Crypto]:
         pass
 
     @abstractmethod
-    def get_coin_with_id(self, id) -> Crypto:
+    def get(self, id) -> Crypto:
         pass
 
     @abstractmethod
-    def get_markets_for_coin_with_id(self, id) -> List[Market]:
+    def get_markets(self, id) -> List[Market]:
         pass
 
     @abstractmethod
-    def get_historical_price_for_coin_with_id(
+    def get_historical_price(
             self,
             id: str,
             interval: str,
