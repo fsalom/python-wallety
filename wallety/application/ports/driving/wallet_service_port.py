390160bd6d5927c97ffda23783942f7b049ec936
Fernando Salom <fer@rudo.es>
2025-05-27 20:39:18 +0200

    Fix entity route

--- wallety/application/ports/driving/wallet_service_port.py
+++ wallety/application/ports/driving/wallet_service_port.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from wallety.domain.entities.wallet import Wallet
+from wallety.domain.wallet import Wallet
 
 
 class WalletServicePort(ABC):
