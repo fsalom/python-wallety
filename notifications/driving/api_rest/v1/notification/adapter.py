655e53c4279888419d1c19fdd865b10e6c28c863
Fernando Salom <fer@rudo.es>
2025-05-31 20:40:01 +0200

    Fix notification count validation

--- notifications/driving/api_rest/v1/notification/adapter.py
+++ notifications/driving/api_rest/v1/notification/adapter.py
@@ -7,6 +7,8 @@ from starlette.responses import JSONResponse
 from notifications.application.ports.driving.push_notification_service_port import PushNotificationServicePort
 from notifications.driving.api_rest.v1.notification.mapper import NotificationAPIMapper
 from notifications.driving.api_rest.v1.notification.models import NotificationDataRequest
+from dependency_injector.wiring import inject, Provide
+from notifications.infrastructure.di.notification.container import NotificationContainer
 
 fcm_router = APIRouter()
 
@@ -17,7 +19,7 @@ async def send_notification(notification_request: NotificationDataRequest,
                             service: PushNotificationServicePort = Depends(Provide[NotificationContainer.service]),
                             mapper: NotificationAPIMapper = Depends(Provide[NotificationContainer.api_mapper])):
     notification = mapper.from_notification_dto_to_domain(notification_request)
-    if notification.recipients.count() > 1:
+    if len(notification.recipients) > 1:
         service.send_bulk(notification, notification.recipients)
     else:
         service.send(notification, notification.recipients[0])
