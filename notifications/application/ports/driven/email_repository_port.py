ecc96249aa751400f81693f4d99dcaeab02f256c
Fernando Salom <fer@rudo.es>
2025-06-03 17:48:02 +0200

    Add driven adapter port for email

---
+++ notifications/application/ports/driven/email_repository_port.py
@@ -0,0 +1,7 @@
+from notifications.application.ports.driven.notification_repository_port import NotificationRepositoryPort
+
+
+class EmailRepositoryPort(NotificationRepositoryPort):
+    """
+    Driven port for sending email notifications.
+    """
