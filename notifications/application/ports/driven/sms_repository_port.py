4adafc002f7edddf3bb94520020e815cd1c4dfca
Fernando Salom <fer@rudo.es>
2025-06-04 08:18:17 +0200

    Add driven adapter port for sms

---
+++ notifications/application/ports/driven/sms_repository_port.py
@@ -0,0 +1,7 @@
+from notifications.application.ports.driven.notification_repository_port import NotificationRepositoryPort
+
+
+class SMSRepositoryPort(NotificationRepositoryPort):
+    """
+    Driven port for sending SMS notifications.
+    """
