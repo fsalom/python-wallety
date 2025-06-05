6a0eac70dec755338450e69da922a5c69b40517a
Fernando Salom <fer@rudo.es>
2025-06-05 17:44:49 +0200

    Add driving port for email

---
+++ notifications/application/ports/driving/email_notification_service_port.py
@@ -0,0 +1,7 @@
+from notifications.application.ports.driving.notification_service_port import NotificationServicePort
+
+
+class EmailNotificationServicePort(NotificationServicePort):
+    """
+    Driving port for sending email notifications.
+    """
