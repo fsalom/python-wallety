f50ef400464f8dff62da0e7d32ec69941a2ca388
Fernando Salom <fer@rudo.es>
2025-06-05 17:55:21 +0200

    Add driving port for sms

---
+++ notifications/application/ports/driving/sms_notification_service_port.py
@@ -0,0 +1,7 @@
+from notifications.application.ports.driving.notification_service_port import NotificationServicePort
+
+
+class SMSNotificationServicePort(NotificationServicePort):
+    """
+    Driving port for sending SMS notifications.
+    """
