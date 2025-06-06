2e5916ff39726f366349a6a0dfa827ace40f0917
Fernando Salom <fer@rudo.es>
2025-06-06 14:23:14 +0200

    Add new dependencies to notification container

--- notifications/infrastructure/di/notification/container.py
+++ notifications/infrastructure/di/notification/container.py
@@ -1,7 +1,11 @@
 from dependency_injector import containers, providers
 
 from notifications.application.services.push_notification_service import PushNotificationService
+from notifications.application.services.email_notification_service import EmailNotificationService
+from notifications.application.services.sms_notification_service import SMSNotificationService
 from notifications.driven.firebase.adapter import FirebaseRepositoryAdapter
+from notifications.driven.email.adapter import EmailRepositoryAdapter
+from notifications.driven.sms.adapter import SMSRepositoryAdapter
 from notifications.driving.api_rest.v1.notification.mapper import NotificationAPIMapper
 
 
@@ -12,10 +16,20 @@ class NotificationContainer(containers.DeclarativeContainer):
     )
 
     firebase_repository = providers.Factory(FirebaseRepositoryAdapter)
+    email_repository = providers.Factory(EmailRepositoryAdapter)
+    sms_repository = providers.Factory(SMSRepositoryAdapter)
 
     service = providers.Factory(
         PushNotificationService,
         firebase=firebase_repository,
+    )
+    email_service = providers.Factory(
+        EmailNotificationService,
+        email_repository=email_repository,
+    )
+    sms_service = providers.Factory(
+        SMSNotificationService,
+        sms_repository=sms_repository,
     )
 
     api_mapper = providers.Factory(
